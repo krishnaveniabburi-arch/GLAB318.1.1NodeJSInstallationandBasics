16864ef5490fff04841e591d19ba2c73b9619915
krishnaveniabburi-arch <krishnaveni.abburi@gmail.com>
2026-06-11 23:49:05 -0400

    new validation logic

--- index.js
+++ index.js
@@ -1,14 +1,22 @@
 console.log("Hello World");
 
-// NODE PACKAGE MANAGER--PART-4
+// NODE PACKAGE MANAGER--PART-4--creating a node server
 const http = require('http');
 const hostname = '127.0.0.1';
 const port = 3000;
 
+//const server = http.createServer((req, res) => {
+  //res.statusCode = 200;
+  //res.setHeader('Content-Type', 'text/plain');
+  //res.end('Hello World!\n');
+//});
+
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World!\n');
+  res.setHeader('Content-Type', 'text/html');
+  res.write('<h1 style="color: red">Hello World!</h1>');
+  res.write('<p>I wonder what else we can send...</p>');
+  res.end();
 });
 
 server.listen(port, hostname, () => {
